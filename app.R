543072dfb62f7e7f7544ab1e7df60898ce5868f3
leon-fabian <fabian.leon.zaragoza@gmail.com>
2022-04-10 22:37:39 -0500

    structuring shinydashboard approach

--- app.R
+++ app.R
@@ -1,15 +1,15 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Ram Pangaluri, Ashok Shanker, Fabian Leon
+# STAT 689
+# Data Visualization Dashboard
+
+# Resources
+# https://rstudio.github.io/shinydashboard/appearance.html
+# https://rstudio.github.io/shinydashboard/structure.html
 
 library(shiny)
 library(ggplot2)
-
+library(shinydashboard)
+library(plotly)
 
 # Read Dataset
 data = read.csv("data/data.csv")
@@ -27,76 +27,111 @@ data[,integers] = lapply(data[,integers], as.integer)
 usda = data[which(data$Dataset == "USDA"),]
 txar = data[which(data$Dataset == "TXAR"),]
 
+traits = c("Year", "DA", "PH", "EX", "GY")
 
-traits = c("DA", "PH", "EX", "GY")
 
-##### UI ##### 
-ui <- navbarPage("Texas Grain Sorghum",
-                 
-                 # Line Graph - Need a different layout to get rid of empty side panel
-                 tabPanel("Historical Yield Improvements",
-                          sidebarLayout(
-                            sidebarPanel(),
-                            mainPanel(
-                              plotOutput("linegraph"),
-                              sliderInput("slider", label = "Date Range",
-                                          min = min(txar$Year),
-                                          max = max(txar$Year),
-                                          value=c(min(txar$Year), max(txar$Year)))
-                            )
-                          )
-                 ),
-                 
-                 # Scatter
-                 tabPanel("Relationships",
-                          sidebarLayout(
-                            sidebarPanel(
-                              selectInput("scat_x", label = h2("select x-axis"), 
-                                          choices = colnames(txar)),
-                              selectInput("scat_y", label = h2("select y-axis"), 
-                                          choices = colnames(txar))
-                            ),
-                            mainPanel(plotOutput("scatterplot"))
-                          )
-                 ),
-                 
-                 # Chloropleth map         
-                 tabPanel("Map"
-                          
-                 ), 
-                 
-                 
-                 # Racing Bars
-                 tabPanel("Brands"
-                 
-                          ), 
-                 
-                 # LMM Statistical Analysis & Predictions
-                 tabPanel("Dissecting Yield Factors")
+##### UI #####
+ui <- dashboardPage(
+  dashboardHeader(title = "Texas Grain Sorghum"),
+  
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Texas AgriLife Variety Testing", 
+               tabName = "intro", 
+               icon = icon("grain", lib="glyphicon")),
+      menuItem("Historical Yield Improvements", 
+               tabName = "line", 
+               icon = icon("signal", lib="glyphicon")),
+      menuItem("Trait Relationships", 
+               tabName = "traits", 
+               icon = icon("leaf", lib="glyphicon")),
+      menuItem("Map", 
+               tabName = "Map", 
+               icon = icon("globe", lib="glyphicon")),
+      menuItem("Company Brands", 
+               tabName = "brands", 
+               icon = icon("object-align-left", lib="glyphicon")),
+      menuItem("Statistical Analysis", 
+               tabName = "stats", 
+               icon = icon("stats", lib="glyphicon"))
+    )
+  ),
+  
+  dashboardBody(
+    tabItems(
+      
+      # Introduction
+      tabItem(tabName = "intro",
+              h2("Crop testing program at Texas AgriLife")
+              # NEED TO ADD BACKGROUND ABOUT TEXAS AGRILIFE AND USDA AND GRAIN SORGHUM IN TEXAS
+      ),
+      
+      # Line Graph
+      tabItem(tabName = "line",
+              h2("Yield Improvements over the Years"),
+              fluidRow(
+                box(plotlyOutput("graph1"))
+                # ADD BOX WITH A TIMELINE EXPLAINING UPS AND DOWNS
+              )
+      ),
+      
+      # Scatter plots
+      tabItem(tabName = "traits",
+              h2("Correlations between metrics of hybrid performance"),
+              fluidRow(
+                box(selectInput("scat_x", label = h2("select x-axis"), 
+                                choices = traits),
+                    selectInput("scat_y", label = h2("select y-axis"), 
+                                choices = traits)),
+                box(plotOutput("scatter", height = 300))
+              )
+              # NEED TO DISPLAY PEARSON'S R AND/OR PLOT SMOOTHED LINE TO THE RELATIONSHIP
+      ),
+      
+      # Chloropleth map
+      tabItem(tabName = "Map"
+              #h2("Chloropleth map of average yield per county")
+      ),
+      
+      # Racing Bars
+      tabItem(tabName = "brands",
+              h2("Accumulated numbers of Hybrids submitted over the years")
+      ),
+      
+      # LMM Statistical Analysis & Predictions
+      tabItem(tabName = "stats",
+              h2("Linear mixed model for factors influencing grain yields")
+      )
+    )
+  )
 )
 
 
 
-# Server # 
-server <- function(input, output) {
+
+####### Server #########
+server = function(input, output) {
   
-  # Line Graph 
-    output$linegraph = renderPlot({
-      ggplot(txar, aes(x = Year, y = GY)) + 
-      geom_smooth() +
-      coord_cartesian(xlim = input$slider) + 
-      theme_minimal()
+  # Line Graph
+  output$graph1 = renderPlotly({
+    p = ggplot(txar, aes(x = Year, y = GY)) +
+      geom_smooth()
+    ggplotly(p)
   })
   
-  
-  # Scatterplot
-  output$scatterplot <- renderPlot({
-    ggplot(txar, aes_string(x = input$scat_x, y = input$scat_y)) + 
+  # Scatter Plot
+  output$scatter = renderPlot({
+    ggplot(txar, aes_string(x=input$scat_x, y=input$scat_y)) + 
       geom_point()
   })
   
   # Chloropleth map
   
+  
+  # Racing Bars
+  
+
+  # LMM Statistical Analysis & Predictions
   
   
 }
